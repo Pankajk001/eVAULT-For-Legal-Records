5b7cd2b097b9e407056d17b81db163f0615dabb2
SARTHAK KHARE <113530791+the-code-machine@users.noreply.github.com>
2024-05-12 10:50:34 +0530

    Create Loader.jsx

---
+++ src/components/utlis/Loader.jsx
@@ -0,0 +1,10 @@
+export default () => {
+    return (
+      <div className="flex h-screen items-center w-screen fixed top-0 left-0 z-[100000000000] justify-center bg-white bg-opacity-80">
+        <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-[#792938] border-t-transparent"></div>
+      </div>
+    );
+  };
+  
+
+  
