a026a5d0df1c577f2f5065bcd7ec345c855cceed
Raksha kirar <138713301+Raksha1001@users.noreply.github.com>
2024-05-12 07:59:27 +0530

    Create Dashboard.js

---
+++ src/components/data/Dashboard.js
@@ -0,0 +1,23 @@
+export const Dashboard = [
+    {
+        name: 'Legal Records',  
+        href: '/dashBoard/legalRecords',
+        innerItems: [
+            {
+                name: 'Upload Documents',
+                href: '/dashBoard/legalRecords/uploadDocuments',
+            },
+            {
+                name: 'View Documents',
+                href: '/dashBoard/legalRecords/viewDocuments',
+            },
+            {
+                name: 'Modify Documents',
+                href: '/dashBoard/legalRecords/modifyDocuments',
+            },
+           
+        ]
+      
+    },
+  
+]
