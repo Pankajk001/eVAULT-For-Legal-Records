8a348936c6202cce99a6d7060cb815d53399d3a1
SARTHAK KHARE <113530791+the-code-machine@users.noreply.github.com>
2024-05-12 07:52:52 +0530

    Create page.js

---
+++ src/app/dashBoard/page.js
@@ -0,0 +1,13 @@
+'use client'
+import DefaultLayout from '@/components/other/DefaultLayout'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <DefaultLayout>
+          <h1>hello</h1>
+        </DefaultLayout>
+    </div>
+  )
+}
