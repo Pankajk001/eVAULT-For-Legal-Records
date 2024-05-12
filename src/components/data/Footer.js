9ca6254b02b1e365c7e2f8f9b81fc30e242695ba
Raksha kirar <138713301+Raksha1001@users.noreply.github.com>
2024-05-12 08:00:48 +0530

    Create Footer.js

---
+++ src/components/data/Footer.js
@@ -0,0 +1,57 @@
+export const footerNavs = [
+    {
+        label: "Important Links",
+        items: [
+            {
+                href: '',
+                name: 'About'
+            },
+            {
+                href: '',
+                name: 'Contact'
+            },
+            {
+                href: '',
+                name: 'Features'
+            },
+           
+        ],
+    },
+    {
+        label: "Legal Records",
+        items: [
+            {
+                href: '',
+                name: 'Upload Document'
+            },
+            {
+                href: '',
+                name: 'Modify Document'
+            },
+            {
+                href: '',
+                name: 'View Document'
+            },
+         
+        ]
+    },
+    {
+        label: "Articles",
+        items: [
+            {
+                href: '',
+                name: 'No Paper Files'
+            },
+            {
+                href: '',
+                name: 'Digital India'
+            },
+            {
+                href: '',
+                name: 'Blockchain in Judiciary'
+            },
+           
+        ]
+    },
+
+]
