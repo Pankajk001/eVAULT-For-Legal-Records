bdc06608657db55bcc28598ee967c3b0febcf753
Raksha kirar <138713301+Raksha1001@users.noreply.github.com>
2024-05-12 08:01:14 +0530

    Create Hero.js

---
+++ src/components/data/Hero.js
@@ -0,0 +1,36 @@
+export const Hero=[
+  
+    {
+        imgsrc:"https://cdnbbsr.s3waas.gov.in/s3ec0490f1f4972d133619a60c30f3559e/uploads/2024/01/2024011677.svg",
+        links:"SuSwagatam",
+    },
+
+    {
+        imgsrc:"https://cdnbbsr.s3waas.gov.in/s3ec0490f1f4972d133619a60c30f3559e/uploads/2024/01/2024011620.svg",
+        links:"Judges Library",
+    },
+
+    {
+        imgsrc:"https://cdnbbsr.s3waas.gov.in/s3ec0490f1f4972d133619a60c30f3559e/uploads/2024/01/2024011695.svg",
+        links:"Research Planning",
+    },
+    {
+        imgsrc:"https://cdnbbsr.s3waas.gov.in/s3ec0490f1f4972d133619a60c30f3559e/uploads/2024/01/2024011782.svg",
+        links:"GSICC",
+    },
+
+    {
+        imgsrc:"https://cdnbbsr.s3waas.gov.in/s3ec0490f1f4972d133619a60c30f3559e/uploads/2024/01/2024011759.svg",
+        links:"NALSA",
+    },
+    {
+        imgsrc:"https://cdnbbsr.s3waas.gov.in/s3ec0490f1f4972d133619a60c30f3559e/uploads/2024/01/2024011756.svg",
+        links:"SCLSC",
+    },
+    {
+        imgsrc:"https://cdnbbsr.s3waas.gov.in/s3ec0490f1f4972d133619a60c30f3559e/uploads/2024/01/2024011629.svg",
+        links:"Ce-Committee",
+    },
+
+
+]
